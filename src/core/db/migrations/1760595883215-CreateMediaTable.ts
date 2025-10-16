d49ddc8450eec15696b20e42ffdd7b6a1272163f
Claudius A <ayadiclaudius@gmail.com>
2025-10-16 14:51:02 +0100

    feat: add CreateMediaTable migration for media table creation

---
+++ src/core/db/migrations/1760595883215-CreateMediaTable.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateMediaTable1760595883215 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
